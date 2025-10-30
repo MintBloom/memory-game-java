6fd271e480cb1cef26253b506e77c40ae3863d2c
MintBloom <a.stegmann09@gmail.com>
2025-10-30 19:41:23 +0000

    updated MemoryGameJava.java
    base game is now finished. Proper tests still need to be made for exception handling.

--- MemoryGameJava.java
+++ MemoryGameJava.java
@@ -10,9 +10,11 @@ public class MemoryGameJava {
     private static final String HIDDEN = " * ";
     private static String[][] board = new String[SIZE][SIZE];
     private static boolean[][] revealed = new boolean[SIZE][SIZE];
+    private static int attemptCounter = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Memory Pair Game (4x4 : 0,1,2,3)\n");
         initBoard();
         printBoard();
         userInput();
@@ -41,7 +43,6 @@ public class MemoryGameJava {
     }
     private static void printBoard() {
         // this function creates the board with its row and col. numbers in the terminal 
-        System.out.println("Memory Pair Game (4x4 : 0,1,2,3)\n");
         // header
         System.out.print("    ");
         for (int c = 0; c < SIZE; c++) System.out.printf(" %d ", c);
@@ -60,30 +61,38 @@ public class MemoryGameJava {
         System.out.println();
     }
 
-
     public static void userInput() {
         // this function will take the users choice of which cards to reveal
         int matchesFound = 0; // number of card matches found by the user 
         int totalMatchesFound = 8; // obtained via. (SIZE*SIZE/2)
         
         while (matchesFound < totalMatchesFound) {
+            printBoard();
             Scanner sc = new Scanner(System.in);
             System.out.print("Column number: ");
             int column1 = sc.nextInt();
             System.out.print("Row number: ");
             int row1 = sc.nextInt();
             checkUserInput(row1, column1); //sends user inputs to be validated, and if necessary, redone
-
-
+            
+            printBoard();
             System.out.print("Column number: ");
             int column2 = sc.nextInt();
             System.out.print("Row number: ");
             int row2 = sc.nextInt();
             checkUserInput(row2, column2); //sends user inputs to be validated, and if necessary, redone
 
-            checkUserPair(row1, column1, row2, column2);
+            if (board[row1][column1].equals(board[row2][column2])) {
+                matchesFound++;
+                System.out.println("One match found! Keep going!");
+            }
+            else {
+                System.out.println("Not a match unfortunately, Keep going!");
+            }
+            attemptCounter++;
         }
-        System.out.println("Congratulations, You have found all the matching pairs :OOOOO, it took you {} attempts "); // win game statement
+        System.out.println(String.format("Congratulations, You have found all the matching pairs :OOOOO, it took you %d attempts", MemoryGameJava.attemptCounter)); // win game statement
+        
     }
     
     public static boolean checkUserInput(int row, int column){
@@ -103,19 +112,4 @@ public class MemoryGameJava {
         return true;
     }
 
-    public static void checkUserPair(int row1, int column1, int row2, int column2) {
-        //this function will check whether the user has guessed correctly and matched a pair or not
-        if (board[row1][column1] == board[row2][column2]) {
-            
-        }
-    }
-
-    public static void attemptCounter() {
-        // keeps track of attempts needed to complete a game
-        int attempts = 0;
-    }
-
-    public static void timer() {
-        // keeps track of time taken to complete a game
-    }
 }
