3f5a6156514f5e4aa907b4072cc423e624b5d86e
MintBloom <a.stegmann09@gmail.com>
2025-11-01 18:32:32 +0000

    finished base code for game
    Exception handling must still be implemented for non-integer inputs by the user. Otherwise the code is in working order.

--- MemoryGameJava.java
+++ MemoryGameJava.java
@@ -2,7 +2,7 @@
 // Students do not need to use this code in their assessment, fine to junk it and do something different!
 //
 //The cards are A B C D E F G H.
-// A player can pick card’s by entering row and column. For example, ‘First pick (row col): 0 2‘.
+// A player can pick card’s by entering row and column. For example, for the first pick 'row: 2 column: 0'
 import java.util.*;
 
 public class MemoryGameJava {
@@ -11,12 +11,15 @@ public class MemoryGameJava {
     private static String[][] board = new String[SIZE][SIZE];
     private static boolean[][] revealed = new boolean[SIZE][SIZE];
     private static int attemptCounter = 0;
+    private static int row1 = 0;
+    private static int column1 = 0;
+    private static int row2 = 0;
+    private static int column2 = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Memory Pair Game (4x4 : 0,1,2,3)\n");
         initBoard();
-        printBoard();
         userInput();
     }
     public static void clearScreen() {   
@@ -69,47 +72,67 @@ public class MemoryGameJava {
         while (matchesFound < totalMatchesFound) {
             printBoard();
             Scanner sc = new Scanner(System.in);
-            System.out.print("Column number: ");
-            int column1 = sc.nextInt();
-            System.out.print("Row number: ");
-            int row1 = sc.nextInt();
-            checkUserInput(row1, column1); //sends user inputs to be validated, and if necessary, redone
+            System.out.print("Column number1: ");
+            MemoryGameJava.column1 = sc.nextInt();
+            System.out.print("Row number1: ");
+            MemoryGameJava.row1 = sc.nextInt();
+            checkInput1();//sends user inputs to be validated, and if necessary, redone
             
             printBoard();
-            System.out.print("Column number: ");
-            int column2 = sc.nextInt();
-            System.out.print("Row number: ");
-            int row2 = sc.nextInt();
-            checkUserInput(row2, column2); //sends user inputs to be validated, and if necessary, redone
+            System.out.print("Column number2: ");
+            MemoryGameJava.column2 = sc.nextInt();
+            System.out.print("Row number2: ");
+            MemoryGameJava.row2 = sc.nextInt();
+            checkInput2(); //sends user inputs to be validated, and if necessary, redone
 
-            if (board[row1][column1].equals(board[row2][column2])) {
+            if (board[MemoryGameJava.row1][MemoryGameJava.column1].equals(board[MemoryGameJava.row2][MemoryGameJava.column2])) {
+                printBoard();
                 matchesFound++;
-                System.out.println("One match found! Keep going!");
+                System.out.println("Match found! Keep going!");
             }
             else {
-                System.out.println("Not a match unfortunately, Keep going!");
+                printBoard();
+                System.out.println("Not a match unfortunately, keep going!");
+                revealed[MemoryGameJava.row1][MemoryGameJava.column1] = false;
+                revealed[MemoryGameJava.row2][MemoryGameJava.column2] = false;
             }
-            attemptCounter++;
+            MemoryGameJava.attemptCounter++;
         }
         System.out.println(String.format("Congratulations, You have found all the matching pairs :OOOOO, it took you %d attempts", MemoryGameJava.attemptCounter)); // win game statement
         
     }
     
-    public static boolean checkUserInput(int row, int column){
-        // if anything is wrong with the user's input it will be caught here and told to try another coordinate input
+    public static void  checkInput2(){
+        // corrects user choice for column2 and row2 
         Scanner sc = new Scanner(System.in);
-        while (column < 0 || column > 3 || row < 0 || row > 3 || revealed[row][column] == true) { 
-            /*should any of these conditions be true then the user input is FALSE and must be redone if ALL CONDITIONS ARE FALSE then the user input is TRUE and passses validation*/
+        while (column2 < 0 || column2 > 3 || row2 < 0 || row2 > 3 || revealed[row2][column2] == true) { 
+            //should any of these conditions be true then the user input is FALSE and must be redone
+            //if ALL CONDITIONS ARE FALSE then the user input is TRUE and passes
             clearScreen();
-            System.out.print("Incorrect input or the card you have chosen has aleady been revealed! Try Again.");
+            System.out.println("Incorrect input or the card you have chosen has aleady been revealed! Try Again.");
             printBoard();
             System.out.print("\nColumn number: ");
-            column = sc.nextInt();
+            column2 = sc.nextInt();
             System.out.print("Row number: ");
-            row = sc.nextInt();
+            row2 = sc.nextInt();
         }
-        revealed[row][column] = true;
-        return true;
+        revealed[row2][column2] = true;
     }
 
+    public static void  checkInput1(){
+        // corrects user choice for column1 and row1 
+        Scanner sc = new Scanner(System.in);
+        while (column1 < 0 || column1 > 3 || row1 < 0 || row1 > 3 || revealed[row1][column1] == true) { 
+            //should any of these conditions be true then the user input is FALSE and must be redone
+            //if ALL CONDITIONS ARE FALSE then the user input is TRUE and passes
+            clearScreen();
+            System.out.println("Incorrect input or the card you have chosen has aleady been revealed! Try Again.");
+            printBoard();
+            System.out.print("\nColumn number: ");
+            column1 = sc.nextInt();
+            System.out.print("Row number: ");
+            row1 = sc.nextInt();
+        }
+        revealed[row1][column1] = true;
+    }
 }
